995d6608662b4986c6e277bc0a54427dc5209bf9
Chris Gallo <chzvalrmd17@gmail.com>
2022-08-24 08:07:44 -0600

    started express and established MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const colors = require('colors')
+const connectDB = require('./config/db')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// app.use(errorHandler)
+
+app.listen(500, () => console.log(`Server started on port ${port}`))
