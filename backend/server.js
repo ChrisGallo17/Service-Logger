8b6892482ffaa5664b26106935844e31105a4e72
Chris Gallo <chzvalrmd17@gmail.com>
2022-09-07 18:56:29 -0600

    CRUD for REST API

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
-const express = require('express')
-const colors = require('colors')
-const {errorHandler} = require("./middleware/errorMiddleware")
-// const connectDB = require('./config/db')
-const dotenv = require('dotenv').config()
-const port = process.env.PORT || 5000
+const path = require('path');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
 
-// connectDB()
+
+connectDB()
 
 const app = express()
-
-app.use('/api/cars', require('./routes/getRoutes'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(errorHandler)
+app.use('/api/car', require('./routes/getRoutes'))
 
+app.use(errorHandler)
 
 app.listen(5000, () => console.log(`Server started on port ${port}`))
